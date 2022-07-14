71540c2a94b708a7fb97db2b9ee5d2f88a935255
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-07-14 19:23:12 -0300

    criação de repositorio node-orm-express

---
+++ api/routes/index.js
@@ -0,0 +1,9 @@
+const bodyParser = require('body-parser');
+const pessoas = require('./pessoasRoute.js')
+const turmas = require('./turmasRoute.js')
+const niveis = require('./niveisRoute.js')
+
+module.exports = app => {
+  app.use(bodyParser.json())
+  app.use(pessoas, turmas, niveis) 
+}
---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,21 @@
+const {Router} = require ('express')
+const PessoaController = require('../controllers/PessoaController.js')
+
+
+const router = Router()
+
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegarPessoaPorId)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+router.post('/pessoas/:estudateId/matricula', PessoaController.criaMatricula)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deletarMatricula)
+router.delete('/pessoas/:id', PessoaController.deletarPessoa)
+
+module.exports = router
+
+
+'/pessoas/:estudateId/matricula'
+'/pessoas/:estudateId/matricula'
