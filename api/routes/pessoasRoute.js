cdeae90b538ec0c70fdfa93bd2f3f75e437f6d57
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-07-20 17:00:54 -0300

    ....p

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,5 +1,7 @@
 const {Router} = require ('express')
 const PessoaController = require('../controllers/PessoaController.js')
+const MatriculaController = require('../controllers/MatriculaController.js')
+
 
 
 const router = Router()
@@ -9,14 +11,25 @@ router.get('/pessoas/ativa', PessoaController.pegaPessoasAtivas)
 router.get('/pessoas/:id', PessoaController.pegarPessoaPorId)
 router.get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas)
 
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', MatriculaController.pegaUmaMatricula)
+router.get('/pessoas/:estudanteId/matricula', MatriculaController.pegaMatricula)
+router.get('/pessoas/matricula/:turmaId/confirmadas', MatriculaController.pegaMatriculaPorTurma)
+
 router.post('/pessoas', PessoaController.criaPessoa)
 router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
 router.post('/pessoas/:estudanteId/cancela', PessoaController.cancelaPessoa)
 
+router.post('/pessoas/:estudateId/matricula', MatriculaController.criaMatricula)
+router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', MatriculaController.restauraMatricula)
+
 
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', MatriculaController.atualizaMatricula)
+
 
 router.delete('/pessoas/:id', PessoaController.deletarPessoa)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', MatriculaController.deletarMatricula)
+
 
 module.exports = router
 
